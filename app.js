5e6cb92b176e86b1cffce942b81c2bd9db870fce
indimon <90929596+indimon@users.noreply.github.com>
2022-04-30 16:44:47 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var routes = require("./routes");
 
 var app = express();
 
-app.set("port", process.env.PORT || 8080);
+app.set("port", process.env.PORT || 8000);
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
